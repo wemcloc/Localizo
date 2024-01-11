44d43460388abd27c781d775430b50c384ddb236
wemcloc <156009556+wemcloc@users.noreply.github.com>
2024-01-11 11:41:44 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([11.4095587,-81.0234022],13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
